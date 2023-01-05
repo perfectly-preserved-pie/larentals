40a239f6ba9cebf7dc451fb79ce3a4184a8855ac
Sahib Bhai <lunarwolf01@gmail.com>
2023-01-05 04:52:16 +0000

    Add some hot red text for the warning banner

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ app = Dash(
 )
 
 # Set the page title
-app.title = "WhereToLive.LA"
+app.title = "WhereToLive.LA - DEVELOPMENT & TEST INSTANCE"
 app.description = "An interactive map of rental properties in Los Angeles County."
 
 # For Gunicorn
@@ -861,10 +861,10 @@ last_updated = df['date_processed'].max().strftime('%m/%d/%Y')
 
 title_card = dbc.Card(
   [
-    html.H3("WhereToLive.LA - DEV & TEST INSTANCE", className="card-title"),
+    html.H3("WhereToLive.LA - DEVELOPMENT & TEST INSTANCE", className="card-title"),
     html.P("An interactive map of rental properties in Los Angeles County. Updated weekly."),
-    html.P("⚠ This is a DEVELOPMENT instance of WhereToLive.LA. Information here may be missing, incomplete, or inaccurate."),
-    html.P("Please use the main website at https://wheretolive.LA for the most up-to-date information."),
+    html.P("⚠ WARNING: This is a TEST version of WhereToLive.LA. Information here may be missing, incomplete, or inaccurate. Or parts of the website may not work at all. ⚠", style={"color": "red"}),
+    html.P(["⚠ Please use the main website at ", html.A("https://wheretolive.LA", href="https://wheretolive.LA"), " for the most up-to-date information. ⚠"], style={"color": "red"}),
     html.P(f"Last updated: {last_updated}"),
     html.I( # use a GitHub icon for my repo
       className="bi bi-github",
