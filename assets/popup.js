b6c5effdd51ee31c09bf78a26b0eefb197f25dc3
Sahib Bhai <lunarwolf01@gmail.com>
2023-08-26 22:19:20 -0700

    Revert "Reduce popup size"
    This reverts commit 3fddc2318d23749d7de08c2b2f4831f62fa1c9a6.

--- assets/popup.js
+++ assets/popup.js
@@ -13,10 +13,10 @@ window.dash_props = Object.assign({}, window.dash_props, {
                     // https://leafletjs.com/reference.html#popup-option
                     //autoPan: false,
                     //closeButton: false,
-                    // Set the maxHeight to 450px if the device is mobile, otherwise use the default value
-                    maxHeight: window.innerWidth < 768 ? 450 : 650, 
-                    // Set the maxWidth to 400px if the device is mobile, otherwise use the default value
-                    maxWidth: window.innerWidth < 768 ? 400 : 300,
+                    // Set the maxHeight to 500px if the device is mobile, otherwise use the default value
+                    maxHeight: window.innerWidth < 768 ? 500 : 650, 
+                    // Set the maxWidth to 175px if the device is mobile, otherwise use the default value
+                    maxWidth: window.innerWidth < 768 ? 175 : 300,
                 })
             }
         }
