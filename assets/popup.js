87ec874167b12173d7312359bddf1eaf16c10ec6
Sahib Bhai <lunarwolf01@gmail.com>
2023-08-26 22:26:52 -0700

    Reduce popup height so it doesn't autoclose when panning

--- assets/popup.js
+++ assets/popup.js
@@ -13,8 +13,8 @@ window.dash_props = Object.assign({}, window.dash_props, {
                     // https://leafletjs.com/reference.html#popup-option
                     //autoPan: false,
                     //closeButton: false,
-                    // Set the maxHeight to 500px if the device is mobile, otherwise use the default value
-                    maxHeight: window.innerWidth < 768 ? 500 : 650, 
+                    // Set the maxHeight to 375px if the device is mobile, otherwise use the default value
+                    maxHeight: window.innerWidth < 768 ? 375 : 650, 
                     // Set the maxWidth to 175px if the device is mobile, otherwise use the default value
                     maxWidth: window.innerWidth < 768 ? 175 : 300,
                 })
