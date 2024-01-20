b3e8fd1a0910858b28c048f443f2b4bec5127707
Sahib Bhai <lunarwolf01@gmail.com>
2024-01-20 14:21:57 -0800

    Alphabetize oil well popup content

--- assets/oil.js
+++ assets/oil.js
@@ -10,12 +10,12 @@ window.myNamespace = Object.assign({}, window.myNamespace, {
             // Check if the required properties exist and create the popup content
             if (feature.properties) {
                 var popupContent = '<h4>Oil Well Info</h4>';
-                popupContent += 'Well Operator: ' + (feature.properties.OperatorNa || 'N/A') + '<br>';
                 popupContent += 'API Number: ' + (feature.properties.API || 'N/A') + '<br>';
-                popupContent += 'Well Type: ' + (feature.properties.WellTypeLa || 'N/A') + '<br>';
                 popupContent += 'Lease Name: ' + (feature.properties.LeaseName || 'N/A') + '<br>';
                 popupContent += 'Start Date: ' + (feature.properties.SpudDate || 'N/A') + '<br>';
+                popupContent += 'Well Operator: ' + (feature.properties.OperatorNa || 'N/A') + '<br>';
                 popupContent += 'Well Status: ' + (feature.properties.WellStatus || 'N/A') + '<br>';
+                popupContent += 'Well Type: ' + (feature.properties.WellTypeLa || 'N/A') + '<br>';
                 
                 marker.bindPopup(popupContent);
             }
