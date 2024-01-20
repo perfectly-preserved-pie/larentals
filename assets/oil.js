a413d4e6ddc1b1692405bf2886563dc3307399d1
Sahib Bhai <lunarwolf01@gmail.com>
2024-01-20 14:21:07 -0800

    Remove comment

--- assets/oil.js
+++ assets/oil.js
@@ -2,7 +2,7 @@ window.myNamespace = Object.assign({}, window.myNamespace, {
     mySubNamespace: {
         drawCustomIcon: function(feature, latlng) {
             const customIcon = L.icon({
-                iconUrl: '/assets/oil_derrick_icon.png',  // URL to your custom icon
+                iconUrl: '/assets/oil_derrick_icon.png',
                 iconSize: [20, 20]  // Adjust the size as needed
             });
             var marker = L.marker(latlng, {icon: customIcon});
