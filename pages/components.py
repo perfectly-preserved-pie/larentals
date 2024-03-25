97a567e1c09a0b0a931a7f42df7ec139517bad55
Sahib Bhai <lunarwolf01@gmail.com>
2024-03-25 09:55:44 -0700

    8092f4870fb9ac1be89fec4ce2d64ac9c3391e84

--- pages/components.py
+++ pages/components.py
@@ -726,10 +726,10 @@ class LeaseComponents(BaseClass):
                         dcc.RadioItems(
                             id='key_deposit_missing_radio',
                             options=[
-                                {'label': 'Yes', 'value': 'True'},
-                                {'label': 'No', 'value': 'False'}
+                                {'label': 'Yes', 'value': True},
+                                {'label': 'No', 'value': False}
                             ],
-                            value='True',
+                            value=True,
                             inputStyle={
                                 "margin-right": "5px",
                                 "margin-left": "5px"
