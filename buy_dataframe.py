34ddfec1b2217c9fc7299f2e0de92084cfae4295
Sahib Bhai <lunarwolf01@gmail.com>
2024-08-19 12:28:40 -0700

    Handle error when saving combined dataframe as parquet file

--- buy_dataframe.py
+++ buy_dataframe.py
@@ -265,7 +265,16 @@ for row in outside_ca_rows.itertuples():
   df_combined.at[row.Index, 'Latitude'] = coordinates[0]
   df_combined.at[row.Index, 'Longitude'] = coordinates[1]
 # Save the new combined dataframe
-df_combined.to_parquet(path="assets/datasets/buy.parquet")
+try:
+  df_combined.to_parquet(path="assets/datasets/buy.parquet")
+except Exception as e:
+  logger.warning(f"Error saving the combined dataframe as a parquet file: {e}. Falling back to CSV...")
+  # Save the new combined dataframe to a CSV file
+  try:
+    df_combined.to_csv(path_or_buf="assets/datasets/buy.csv", index=False)
+    logger.info("Saved the combined dataframe to a CSV file")
+  except Exception as e:
+    logger.error(f"Error saving the combined dataframe to a CSV file: {e}")
 
 # Reclaim space in ImageKit
 reclaim_imagekit_space(df_path="assets/datasets/buy.parquet", imagekit_instance=imagekit)
