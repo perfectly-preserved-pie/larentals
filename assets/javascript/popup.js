d15cd3b4de8c4a03b54b3b1a3b9a90a6b1da9700
Sahib Bhai <lunarwolf01@gmail.com>
2024-08-19 22:52:13 -0700

    Initial work for JavaScript rendering of popups. kinda fucked up atp

--- assets/javascript/popup.js
+++ assets/javascript/popup.js
@@ -5,19 +5,38 @@ window.dash_props = Object.assign({}, window.dash_props, {
     module: {
         on_each_feature: function(feature, layer, context) {
             if (!feature.properties) {
-                return
+                return;
             }
-            if (feature.properties.popup) {
-                layer.bindPopup(feature.properties.popup, {
-                    // Here you can customize the popup
-                    // https://leafletjs.com/reference.html#popup-option
-                    //autoPan: false,
-                    //closeButton: false,
-                    // Set the maxHeight to 375px if the device is mobile, otherwise use the default value
-                    maxHeight: window.innerWidth < 768 ? 375 : 650, 
-                    // Set the maxWidth to 175px if the device is mobile, otherwise use the default value
+            if (feature.properties.data) {
+                const data = feature.properties.data;
+                const popupContent = `
+                    <div>
+                        <h4>${data.address}</h4>
+                        <p>MLS Number: ${data.mls_number}</p>
+                        <p>List Price: ${data.list_price}</p>
+                        <p>Bedrooms: ${data.bedrooms}</p>
+                        <!-- <p>Bathrooms: ${data.bathrooms}</p> -->
+                        <p>Square Feet: ${data.sqft}</p>
+                        <p>Year Built: ${data.year_built}</p>
+                        <p>Price per Sqft: ${data.price_per_sqft}</p>
+                        <p>Listed Date: ${data.listed_date}</p>
+                        <p>Subtype: ${data.subtype}</p>
+                        <p>Pet Policy: ${data.pet_policy}</p>
+                        <p>Terms: ${data.terms}</p>
+                        <p>Garage Spaces: ${data.garage_spaces}</p>
+                        <p>Furnished: ${data.furnished}</p>
+                        <p>Security Deposit: ${data.security_deposit}</p>
+                        <p>Pet Deposit: ${data.pet_deposit}</p>
+                        <p>Key Deposit: ${data.key_deposit}</p>
+                        <p>Other Deposit: ${data.other_deposit}</p>
+                        <p>Laundry: ${data.laundry}</p>
+                        <img src="${data.image_url}" alt="Property Image" style="width:100%;height:auto;">
+                    </div>
+                `;
+                layer.bindPopup(popupContent, {
+                    maxHeight: window.innerWidth < 768 ? 375 : 650,
                     maxWidth: window.innerWidth < 768 ? 175 : 300,
-                })
+                });
             }
         }
     }
