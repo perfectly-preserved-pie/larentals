542507aeb7c67d60a2e992856507d46d5dc61960
Sahib Bhai <lunarwolf01@gmail.com>
2024-11-19 13:13:25 -0800

    Revert "Enable searchable attribute for subtype checklist and add prevent focus on the multiselect input"
    This reverts commit 77d8e69fc76017d87194a2e2cc3f0c3e48b2ccae.

--- assets/javascript/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const multiSelectInput = document.querySelector('.mantine-MultiSelect-input');
-    if (multiSelectInput) {
-        multiSelectInput.addEventListener('focus', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-        });
-    }
-});
