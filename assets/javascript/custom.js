8799cccee46284ffbc0a0e71aec70ef24bc410c3
Sahib Bhai <lunarwolf01@gmail.com>
2024-11-19 12:39:37 -0800

    Revert "Add event listener to prevent focus propagation on MultiSelect dropdown"
    This reverts commit 0707a217cb1356f1f893746fb39d3007dcdad934.

--- assets/javascript/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const multiSelectDropdown = document.querySelector('.mantine-MultiSelect-dropdown');
-    if (multiSelectDropdown) {
-        multiSelectDropdown.addEventListener('focus', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-        });
-    }
-});
