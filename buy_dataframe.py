99d8092d62e060083889f5a373f5117776bd35fa
Sahib Bhai <lunarwolf01@gmail.com>
2025-01-03 09:41:12 -0800

    remove async crap

--- buy_dataframe.py
+++ buy_dataframe.py
@@ -9,7 +9,6 @@ from geopy.geocoders import GoogleV3
 from imagekitio import ImageKit
 from loguru import logger
 from numpy import NaN
-import asyncio
 import glob
 import os
 import pandas as pd
@@ -155,7 +154,7 @@ df["full_street_address"] = df["street_number"] + ' ' + df["street_name"].str.st
 # Iterate through the dataframe and get the listed date and photo for rows 
 for row in df.itertuples():
   mls_number = row[1]
-  webscrape = asyncio.run(webscrape_bhhs(url=f"https://www.bhhscalifornia.com/for-sale/{mls_number}-t_q;/", row_index=row.Index, mls_number=mls_number, total_rows=len(df)))
+  webscrape = webscrape_bhhs(url=f"https://www.bhhscalifornia.com/for-sale/{mls_number}-t_q;/", row_index=row.Index, mls_number=mls_number, total_rows=len(df))
   df.at[row.Index, 'listed_date'] = webscrape[0]
   df.at[row.Index, 'mls_photo'] = imagekit_transform(webscrape[1], row[1], imagekit_instance=imagekit)
   df.at[row.Index, 'listing_url'] = webscrape[2]
