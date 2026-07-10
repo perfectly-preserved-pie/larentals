882cb5d6d9670fd7e2f54f5e93221c191314f441
Sahib Bhai <lunarwolf01@gmail.com>
2026-07-09 20:56:31 -0700

    Change color scheme for ALPR layer

--- assets/js/additional_layers/layers/alpr_cameras.js
+++ assets/js/additional_layers/layers/alpr_cameras.js
@@ -15,8 +15,8 @@
     const FOV_SPREAD_DEGREES = 50;
     const FOV_ARC_STEPS = 10;
     const EARTH_RADIUS_METERS = 6371000;
-    const FOV_FILL_COLOR = "#ffb703";
-    const FOV_OUTLINE_COLOR = "#0b3f43";
+    const FOV_FILL_COLOR = "#f59e0b";
+    const FOV_OUTLINE_COLOR = "#b42318";
     const CCTV_ICON_SVG = `
         <svg class="alpr-camera-cctv-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16.75 12h3.632a1 1 0 0 1 .894 1.447l-2.034 4.069a1 1 0 0 1-1.708.134l-2.124-2.97m1.696-5.627a1 1 0 0 1 .447 1.341l-3.106 6.211a1 1 0 0 1-1.342.447L3.61 12.3a2.92 2.92 0 0 1-1.3-3.91L3.69 5.6a2.92 2.92 0 0 1 3.92-1.3zM2 19h3.76a2 2 0 0 0 1.8-1.1L9 15m-7 6v-4m5-8h.01"></path>
@@ -265,12 +265,12 @@
         return L.geoJSON(hull, {
             interactive: false,
             style: {
-                color: "#115b60",
+                color: "#20242a",
                 weight: 2,
                 opacity: 0.88,
                 dashArray: "6 4",
-                fillColor: "#f2c94c",
-                fillOpacity: 0.1,
+                fillColor: "#f59e0b",
+                fillOpacity: 0.12,
             },
         });
     }
